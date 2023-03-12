581d234c7f48a858c423050264a4650ea2b105b8
Nishikubo-Masato <masato19991020@gmail.com>
2023-03-12 15:58:17 +0900

    groupsを追加

--- DataStructure/UnionFind/unionfind.cpp
+++ DataStructure/UnionFind/unionfind.cpp
@@ -5,7 +5,7 @@ class UnionFind
 {
 public:
     UnionFind() = default;
-    explicit UnionFind(size_t n) : par(n), rank(n, 0), siz(n, 1)
+    explicit UnionFind(size_t n) : _n(n), par(n), rank(n, 0), siz(n, 1)
     {
         iota(par.begin(), par.end(), 0);
     }
@@ -17,8 +17,9 @@ public:
     }
     void unite(int u, int v)
     {
-        u = root(u);
-        v = root(v);
+        assert(0 <= u and u < _n);
+        assert(0 <= v and v < _n);
+        u = root(u), v = root(v);
         if (u != v)
         {
             if (rank[u] < rank[v])
@@ -41,8 +42,33 @@ public:
     {
         return siz[root(pos)];
     }
+    vector<vector<int>> groups()
+    {
+        vector<int> root_buf(_n), group_size(_n);
+        for (int i = 0; i < _n; i++)
+        {
+            root_buf[i] = root(i);
+            group_size[root_buf[i]]++;
+        }
+        vector<vector<int>> result(_n);
+        for (int i = 0; i < _n; i++)
+        {
+            result[i].reserve(group_size[i]);
+        }
+        for (int i = 0; i < _n; i++)
+        {
+            result[root_buf[i]].push_back(i);
+        }
+        result.erase(
+            std::remove_if(result.begin(), result.end(),
+                           [&](const std::vector<int> &v)
+                           { return v.empty(); }),
+            result.end());
+        return result;
+    }
 
 private:
+    int _n;
     vector<int> par, rank, siz;
 };
 
